1814456595affb526bd19e28790dd31dbb7f972e
Angrymouse <rykov.nikas@gmail.com>
2023-07-27 06:58:12 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const solanaWeb3 = require('@solana/web3.js');
+
+
+module.exports = {
+    id: "solana",
+    setup: async () => {
+        const connection = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl('mainnet-beta'));
+        return {
+            async readTxById(id) {
+                const transaction = await connection.getParsedTransaction(id, "finalized");
+                const transferInstruction = transaction.transaction.message.instructions.find(
+                    (instruction) => instruction.program == "system" && instruction.parsed.type == "transfer"
+                );
+                if (!transferInstruction) { return null }
+                return {
+                    coin: "SOL",
+                    timestamp: transaction?.blockTime,
+                    amount: transferInstruction.parsed.info.lamports,
+                    from: transferInstruction.parsed.info.source,
+                    to: transferInstruction.parsed.info.destination,
+                }
+            }
+        }
+    }
+}
