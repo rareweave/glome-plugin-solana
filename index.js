2dfc84079444070e34412d5e102a25c75fa9af54
Angrymouse <rykov.nikas@gmail.com>
2023-09-18 21:15:57 +0200

    fix insecure tx resolving

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ module.exports = {
         return {
             async readTxById(id) {
                 const transaction = await connection.getParsedTransaction(id, "finalized");
-                const transferInstruction = transaction.transaction.message.instructions.find(
-                    (instruction) => instruction.program == "system" && instruction.parsed.type == "transfer"
+                const transferInstruction = (transaction?.transaction?.message?.instructions || []).find(
+                    (instruction) => instruction?.program == "system" && instruction?.parsed?.type == "transfer"
                 );
                 if (!transferInstruction) { return null }
                 return {
                     coin: "SOL",
                     timestamp: transaction?.blockTime,
-                    amount: transferInstruction.parsed.info.lamports,
-                    from: transferInstruction.parsed.info.source,
-                    to: transferInstruction.parsed.info.destination,
+                    amount: transferInstruction?.parsed?.info?.lamports,
+                    from: transferInstruction?.parsed.info?.source,
+                    to: transferInstruction?.parsed?.info?.destination,
                 }
             }
         }
