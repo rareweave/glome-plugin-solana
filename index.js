7649c8a95634458f8e0714499a7025da628361e6
Angrymouse <rykov.nikas@gmail.com>
2023-10-03 18:33:57 +0200

    add verifySignature

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const solanaWeb3 = require('@solana/web3.js');
-
-
+const nacl=require("tweetnacl")
+const bs58=require("bs58")
 module.exports = {
     id: "solana",
     setup: async () => {
+
         const connection = new solanaWeb3.Connection(global.config.gateways.solanaRpc);
         return {
+            async verifySignature(signerAddress, message, signature) {
+                return await nacl
+                    .sign
+                    .detached
+                    .verify(
+                        new TextEncoder().encode(message),
+                        bs58.decode(signature),
+                        bs58.decode(signerAddress)
+                    )
+            },
             async readTxById(id) {
                 const transaction = await connection.getParsedTransaction(id, "finalized");
                 const transferInstruction = (transaction?.transaction?.message?.instructions || []).find(
